8efd3c1f1df16273b46b4c1580fc18239037c76f
Illumination111 <Illumination111@users.noreply.github.com>
2026-08-11 06:57:55 +0000

    deploy: de0422abce207505f11952475c5e60472ef22ea8

--- searcher-09f2665d.js
+++ searcher-09f2665d.js
@@ -440,7 +440,7 @@ window.search = window.search || {};
         if (yes) {
             loadSearchScript(
                 window.path_to_searchindex_js ||
-                path_to_root + 'searchindex-c8db3f6b.js',
+                path_to_root + 'searchindex-88d0cc7c.js',
                 'mdbook-search-index');
             search_wrap.classList.remove('hidden');
             searchicon.setAttribute('aria-expanded', 'true');
